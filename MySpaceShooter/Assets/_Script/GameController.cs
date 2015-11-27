d1e2fdd3044610adeefe716733d9d406d935b983
neargray <henryneargray@yeah.net>
2015-11-27 17:20:57 +0800

    first commit of spaceshooter_unity

---
+++ MySpaceShooter/Assets/_Script/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait = 1.0f;
+    public float waveWait = 2;
+
+
+
+    private Vector3 spawnPosition = Vector3.zero;
+    private Quaternion spawnRotation;
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while(true)
+        {
+            for (int i = 0; i < hazardCount; ++i)
+            {
+                spawnPosition.x = Random.Range(-spawnValues.x, spawnValues.x);
+                spawnPosition.z = spawnValues.z;
+                spawnRotation = Quaternion.identity;
+                Instantiate(hazards, spawnPosition, spawnRotation);
+
+                yield return new WaitForSeconds
+                (spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnWaves());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MySpaceShooter/Assets/_Script/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	// Use this for initialization
+
+    public float speed;
+
+	void Start () {
+
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
